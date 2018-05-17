0695151c78caf00756014fa457ae6086c8ba66bf
zhuxp <nimezhu@gmail.com>
2018-05-17 10:20:32 -0400

    add lib

---
+++ sandNb/bindataServer.go
@@ -0,0 +1,18 @@
+package sandNb
+
+//go:generate go-bindata-assetfs -pkg sandNb app/...
+import (
+	"net/http"
+)
+
+func BindataServer(root string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := root + r.RequestURI
+		bytes, err := Asset(id)
+		if err != nil {
+			w.Write([]byte("file not found"))
+		} else {
+			w.Write(bytes)
+		}
+	})
+}
